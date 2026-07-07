0eb9ce77e8a77048989e7b5904d3f73601f3360c
saadabbasi-playground <saadabbasie@gmail.com>
2026-07-08 08:24:55 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "ironlog-v3";
+const CACHE = "ironlog-v4";
 const ASSETS = ["./", "./index.html", "./data.js", "./icon.png"];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
   self.skipWaiting();
 });
 self.addEventListener("activate", e => {
@@ -14,8 +14,11 @@ self.addEventListener("activate", e => {
 self.addEventListener("fetch", e => {
   e.respondWith(
     fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      // never cache errors (a cached 404 for data.js would brick the app offline)
+      if (res.ok) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      }
       return res;
     }).catch(() => caches.match(e.request).then(m => m || caches.match("./index.html")))
   );
